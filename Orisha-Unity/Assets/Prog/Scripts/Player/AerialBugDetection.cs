b048a11e0128a37d83c163c6f1fc23a841602ba2
PierreGau <p.gauthier09@gmail.com>
2018-05-07 11:03:36 +0200

    Détection de Bug Aerial Lock
    quand le perso reste bloqué entre des collisions et est considéré comme étant en l'air, si il reste bloqué plus de dix secondes il est renvoyé au CheckPoint.
    Script à mettre sur le Player_Transform

---
+++ Orisha-Unity/Assets/Prog/Scripts/Player/AerialBugDetection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vd_Player;
+
+public class AerialBugDetection : MonoBehaviour 
+{
+	Vector3 basePos;
+	float timer = 0.0f;
+	[SerializeField]float debugTime = 2.0f;
+	PlayerController pc;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = debugTime;
+		basePos = transform.position;
+		pc = GetComponentInParent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!pc.IsGrounded) 
+		{
+			if (transform.position.y <= basePos.y - 0.01f) 
+			{
+				timer = debugTime;
+				basePos = transform.position;
+			} 
+			else 
+			{
+				timer -= Time.deltaTime;
+				//Debug.Log ("Timer in action");
+				if (timer <= 0) 
+				{
+					CheckpointsManager.RepopPlayerToCloserCheckpoint ();
+					//Debug.Log ("Get In Place");
+					timer = debugTime;
+				}
+			}
+		}
+		else 
+		{
+			basePos = transform.position;
+			timer = debugTime;
+		}
+	}
+}
