f0e72ef4c7a3cf7a467e4aedd29f63143b70323b
PierreGau <p.gauthier09@gmail.com>
2018-05-17 09:40:58 +0200

    LifeBar: DebugScript
    - Activation/Desactivation de la LifeBar pour la présentation

---
+++ Orisha-Unity/Assets/Prog/Scripts/_Obsolete/LifeBar_Debug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar_Debug : MonoBehaviour {
+
+	[SerializeField] GameObject child;
+
+	// Use this for initialization
+	void Start () {
+		child.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.KeypadDivide)) {
+			child.SetActive (!child.activeInHierarchy);
+		}
+	}
+}
