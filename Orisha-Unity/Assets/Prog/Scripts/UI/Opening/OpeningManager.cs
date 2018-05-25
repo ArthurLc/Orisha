d95d1f25eaad76c0bb8d60b355559b9811249b77
Plastacier <arthur.lacour-travail@laposte.net>
2018-05-25 14:07:32 +0200

    Opening: FadeOut
    - On peux désormais skip l'Opening avec classe !

--- Orisha-Unity/Assets/Prog/Scripts/UI/Opening/OpeningManager.cs
+++ Orisha-Unity/Assets/Prog/Scripts/UI/Opening/OpeningManager.cs
@@ -10,21 +10,29 @@
 
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class OpeningManager : MonoBehaviour {
 
+    [SerializeField] private Image fadeBG;
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private string sceneToLoadAfter = "MainMenu";
 
     private bool isLoadingFinish; // is the loading done??
+    private bool isLaunchReadyToStart; // is the launch ready to start??
+    private bool isFadeStart; // is the fadeToLoad started ??
 
     private AsyncOperation async;
 
     private void Start()
     {
         isLoadingFinish = false;
+        isFadeStart = false;
+
+        fadeBG.color = Color.clear;
+
         StartCoroutine(LoadNewScene());
     }
     private void OnApplicationQuit()
@@ -34,10 +42,19 @@ public class OpeningManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		if(isLoadingFinish && videoPlayer.frame == (long)videoPlayer.frameCount)
+		if(isLoadingFinish)
         {
-            StopAllCoroutines();
-            async.allowSceneActivation = true; // change scene to sceneToLoad
+            if (Input.GetButtonDown(vd_Inputs.InputManager.Pause) || (videoPlayer.frame == (long)videoPlayer.frameCount)) {
+                isLaunchReadyToStart = true;
+            }
+
+            if ((isFadeStart == false) && isLaunchReadyToStart)
+            {
+                isFadeStart = true;
+
+                StopAllCoroutines();
+                StartCoroutine(FadeToLaunchNewScene());
+            }
         }
 	}
 
@@ -55,5 +72,20 @@ public class OpeningManager : MonoBehaviour {
 
             yield return null;
         }
+    }
+    IEnumerator FadeToLaunchNewScene()
+    {
+        float alphaLevel = fadeBG.color.a;
+
+        while (fadeBG.color.a < 1.0f)
+        {
+            alphaLevel += Time.fixedDeltaTime;
+            fadeBG.color = new Color(fadeBG.color.r, fadeBG.color.g, fadeBG.color.b, alphaLevel);
+            videoPlayer.SetDirectAudioVolume(0, 1.0f - alphaLevel);
+            yield return null;
+        }
+
+        async.allowSceneActivation = true; // change scene to sceneToLoad
+        yield return null;
     }
 }
