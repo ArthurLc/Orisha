14395868d3c074afb2d520e18391283205311ca8
Unknown <p.gauthier09@gmail.com>
2018-05-26 17:43:28 +0200

    Brasero : adapté pour les boss

--- Orisha-Unity/Assets/Prog/Scripts/Environment/Brasiers/Brasero.cs
+++ Orisha-Unity/Assets/Prog/Scripts/Environment/Brasiers/Brasero.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class Brasero : MonoBehaviour
 {
-    [SerializeField] ParticleSystem ps;
+    [Header("Particles")]
+    [SerializeField] ParticleSystem mainPs;
+    [SerializeField] ParticleSystem subPs;
+
+    [Header("Sfx")]
     [SerializeField] AudioClip fireLoop;
     [SerializeField] AudioClip fireStart;
 
+    SFX_Object fireLoopSource;
+
+    private void Start()
+    {
+        if(mainPs.gameObject.activeSelf || subPs.gameObject.activeSelf)
+            fireLoopSource = SoundManager.instance.SFX_LoopAtPosition(fireLoop, transform.position, 0, 30);
+    }
+
     public void StartBrasero()
     {
-        if (!ps.gameObject.activeSelf)
+        if (!mainPs.gameObject.activeSelf)
         {
-            ps.gameObject.SetActive(true);
+            mainPs.gameObject.SetActive(true);
             if(fireLoop)
-                SoundManager.instance.SFX_LoopAtPosition(fireLoop, transform.position);
+                fireLoopSource = SoundManager.instance.SFX_LoopAtPosition(fireLoop, transform.position, 0, 30);
             if(fireStart)
+                SoundManager.instance.SFX_PlayOneShot(fireStart);
+        }
+    }
+
+    public void StopBrasero()
+    {
+        if(mainPs)
+            mainPs.gameObject.SetActive(false);
+        if(subPs)
+            subPs.gameObject.SetActive(false);
+
+        if (fireLoopSource)
+            SFX_Pool.Instance.ReturnToPool(fireLoopSource);
+    }
+
+
+    public void StartSubBrasero()
+    {
+        if (!subPs.gameObject.activeSelf)
+        {
+            subPs.gameObject.SetActive(true);
+            if (fireLoop)
+                SoundManager.instance.SFX_LoopAtPosition(fireLoop, transform.position);
+            if (fireStart)
                 SoundManager.instance.SFX_PlayOneShot(fireStart);
         }
     }
