2064a9018e92209249225c6c7e48aa034e022f5e
Unknown <p.gauthier09@gmail.com>
2018-05-27 22:10:48 +0200

    Jean Michel Speed BOI no longer activate checkpoints

--- Orisha-Unity/Assets/Prog/Scripts/Environment/Brasiers/BraseroTriggerActivation.cs
+++ Orisha-Unity/Assets/Prog/Scripts/Environment/Brasiers/BraseroTriggerActivation.cs
@@ -6,7 +6,7 @@ public class BraseroTriggerActivation : MonoBehaviour
     [SerializeField]Brasero[] braseros;
     private void OnTriggerEnter(Collider other)
     {
-        if (braseros.Length > 0)
+        if (braseros.Length > 0 && other.tag == "Player")
         {
             for (int i = 0; i < braseros.Length; i++)
                 braseros[i].StartBrasero();
