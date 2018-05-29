c0fb0da3dd0d2a0b62a47b37500ff2fb5e5613a2
Plastacier <arthur.lacour-travail@laposte.net>
2018-05-29 11:37:00 +0200

    HealItem: Debug & Timer
    - Je viens de te debug au calme Pierre =)
    - Coroutine de 1sec avant de pouvoir récupérer l'orbe de soin quand elle s'active

--- Orisha-Unity/Assets/Prog/Scripts/GFX/HealItem.cs
+++ Orisha-Unity/Assets/Prog/Scripts/GFX/HealItem.cs
@@ -12,6 +12,15 @@ public class HealItem : MonoBehaviour {
 
     [SerializeField] private int healPoints = 10;
     [SerializeField] AudioClip heal;
+    private Collider col;
+
+    private void OnEnable()
+    {
+        col = GetComponent<Collider>();
+        col.enabled = false;
+
+        StartCoroutine(HealTimerApparition(1.0f));
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,8 +32,20 @@ public class HealItem : MonoBehaviour {
             ci.LifeBarHUD.StopAllCoroutines();
             ci.LifeBarHUD.SetActiveHUD(true);
             ci.LifeBarHUD.Invoke("DisableHUD", 2.0f);
-            Destroy(this.gameObject);
             SoundManager.instance.SFX_PlayOneShot(heal);
+            Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator HealTimerApparition(float _timer)
+    {
+        float currentTimer = 0.0f;
+
+        while (currentTimer < _timer)
+        {
+            currentTimer += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        col.enabled = true;
     }
 }
