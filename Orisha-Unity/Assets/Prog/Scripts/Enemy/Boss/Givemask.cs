bf3436b83d0479a9f7ba037eb3dafd804482f4de
PierreGau <p.gauthier09@gmail.com>
2018-05-29 16:04:10 +0200

    Bosses loot Mask

---
+++ Orisha-Unity/Assets/Prog/Scripts/Enemy/Boss/Givemask.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AI_Enemy_Basic))]
+public class Givemask : MonoBehaviour
+{
+    [SerializeField] GameObject MaskToGive;
+
+    AI_Enemy_Basic me;
+    bool spawnMask = false;
+    [SerializeField] float yOffest;
+    [SerializeField] float ySpeed;
+
+    Mask mask;
+    ZoneDiscovery zd;
+    CapsuleCollider c;
+    float yCount = 0;
+    Vector3 startPos;
+	// Use this for initialization
+	void Start ()
+    {
+        me = GetComponent<AI_Enemy_Basic>();
+
+        zd = MaskToGive.GetComponent<ZoneDiscovery>();
+        mask = MaskToGive.GetComponent<Mask>();
+        c = MaskToGive.GetComponent<CapsuleCollider>();
+
+        zd.enabled = mask.enabled = c.enabled = false;
+        MaskToGive.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(me.Health <= 0 && !spawnMask)
+        {
+            spawnMask = true;
+            MaskToGive.SetActive(true);
+            startPos = MaskToGive.transform.position;
+        }
+        else if(spawnMask && yCount < yOffest)
+        {
+            float value = (ySpeed * Time.deltaTime);
+
+            yCount += value;
+
+            MaskToGive.transform.position = new Vector3(MaskToGive.transform.position.x, startPos.y + yCount, MaskToGive.transform.position.z);
+        }
+        else if(yCount >= yOffest)
+        {
+            zd.enabled = mask.enabled = c.enabled = true;
+        }
+	}
+}
