729a6b7b185672abab3f54141b84c01ed84df4d1
Plastacier <arthur.lacour-travail@laposte.net>
2018-05-30 19:51:40 +0200

    PS: FinalCinematic
    - J'ai oublié de push le script de EndGame :p

---
+++ Orisha-Unity/Assets/Prog/Scripts/UI/InGame/EndGameCinematic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameCinematic : MonoBehaviour {
+
+    [SerializeField] private string mainMenuName = "MainMenu";
+    [SerializeField] private bool isMainMenuStartLoading = false;
+
+    private void Start() {
+        isMainMenuStartLoading = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(isMainMenuStartLoading) {
+            SceneManager.LoadScene(mainMenuName);
+        }
+	}
+}
