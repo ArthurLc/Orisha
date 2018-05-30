52751ad36b1b78eab9eff5136cf489c5f5417ff1
Plastacier <arthur.lacour-travail@laposte.net>
2018-05-30 20:08:44 +0200

    LoadingScreen: Debug
    - Le lancement du jeu fonctionne maintenant...

--- Orisha-Unity/Assets/Prog/Scripts/UI/Loading/LoadingScreen_Manager.cs
+++ Orisha-Unity/Assets/Prog/Scripts/UI/Loading/LoadingScreen_Manager.cs
@@ -178,7 +178,7 @@ public class LoadingScreen_Manager : MonoBehaviour
             {
                 LoadingProcess.SetActive(false);
                 LoadingPress_Text.SetActive(true);
-                if (Input.GetButtonDown("StartGame"))
+                if (Input.GetButtonDown("Submit"))
                 {
                     LoadingPress_Text.SetActive(false);
                     LoadingEnd_Text.SetActive(true);
