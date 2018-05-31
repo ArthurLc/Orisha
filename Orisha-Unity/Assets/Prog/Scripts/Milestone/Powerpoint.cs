5a9bac9a9e495df90928775f518548f2c8d6f5e7
PierreGau <p.gauthier09@gmail.com>
2018-05-31 09:58:31 +0200

    Scene Slides

---
+++ Orisha-Unity/Assets/Prog/Scripts/Milestone/Powerpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Powerpoint : MonoBehaviour
+{
+    float timer = 100f;
+    [SerializeField] float slideDuration = 4f;
+    [SerializeField] Sprite[] slides;
+    [SerializeField] Image refCanvas;
+    int id = 0;
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= slideDuration)
+        {
+            refCanvas.sprite = slides[id%slides.Length];
+            ++id;
+            timer = 0f;
+        }
+	}
+}
